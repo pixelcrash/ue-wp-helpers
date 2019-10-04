3d8f33537b586e9d2ac642143fff9cb806705ab4
Georg Kettele <kg@kmkg.org>
2019-10-04 18:59:07 +0100

    Updated
    List sub categories of an array of parents and you can add an ID for the show all page

---
+++ list_categories.php
@@ -0,0 +1,32 @@
+if( ! function_exists('ue_list_categories')){
+    function ue_list_categories($arr=array()){
+      
+      $arr['class'] = ($arr['class']) ? $arr['class']  :  ''; 
+      $all = ($arr['all']) ? 1  :  0; 
+      $cats = "";
+    
+      if($all): 
+        $cats .= "<li><a href='".get_the_permalink($arr['allid'])."' class='current-cat'>All</li>"; 
+      else:
+        $cats .= "<li><a href='".get_the_permalink($arr['allid'])."' class=''>All</li>";
+      endif;
+      
+      if($arr['parents']):
+      foreach($arr['parents'] as $parent):
+      $cats .= wp_list_categories( array(
+            'orderby'    => 'name',
+            'show_count' => true,
+            'hierarchical'        => false,
+            'title_li'            => NULL,
+            'echo'              =>  0,
+            'current_category'    => 0,
+            'child_of'            => $parent
+        )); 
+      endforeach;
+      endif;
+        
+        echo "<ul class='".$arr['class']."' >";
+        echo $cats;
+        echo "</ul>";  
+      }
+}
