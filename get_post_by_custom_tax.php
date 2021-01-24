db2e4192f496bdcbc989c1a17cf9d603281f9c7e
Georg Kettele <kg@kmkg.org>
2021-01-24 13:11:49 +0000

    Create get_post_by_custom_tax.php

---
+++ get_post_by_custom_tax.php
@@ -0,0 +1,29 @@
+<?php
+
+  $terms = get_terms( array(
+    "taxonomy" => "country",
+  ));
+  
+  foreach($terms as $term): 
+  
+  $posts = get_posts([
+  "post_type" => "stockist", 
+  'tax_query' => array(
+    'taxonomy' => 'country',
+    'field' => 'id',
+    'terms' => $term->term_id
+    )]);
+  
+    if($posts):
+      foreach($posts as $post):
+        echo $post->ID . " " . $post->post_title . "<br>;
+      endforeach;
+    else:
+
+    endif;
+  
+  endforeach;
+  
+  
+
+?>
