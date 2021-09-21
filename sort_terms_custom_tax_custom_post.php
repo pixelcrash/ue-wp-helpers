2b6064aec82cd0d5b35b20e85cb9ba92afb6c350
Georg Kettele <kg@kmkg.org>
2021-09-21 12:28:51 +0200

    Update sort_terms_custom_tax_custom_post.php
    If you use a custom taxonomy with a custom post type the get_terms() function sometimes doesn't return the correct count value. Especially if you work with a multilingual system e.f. WPML. This is a workaround to get only the terms from custom post types.
    1. select all the custom_post posts (only ids)
    2. get all the terms 
    3. sort if parent or not
    4. add values
    5. sort values A->Z Parents and Childs

--- sort_terms_custom_tax_custom_post.php
+++ sort_terms_custom_tax_custom_post.php
@@ -1,60 +1,81 @@
-  $ins = get_indications_cLang_parent(); 
-  $cTerms = array();
-  
-  if($ins):
-    
-    // add all parents
-    foreach($ins as $in):
-      if($in->parent):
-        $cTerms[$in->parent]['name'] = get_term( $in->parent )->name;
-        $cTerms[$in->parent]['parent'] = 1;
-      endif;
-    endforeach;
-    
-    // add values
-    foreach($ins as $in):
-      if($in->parent):
-        $newdata = array($in->term_id => $in->name, "name" => $in->name, "child_id" => $in->term_id);
-        //array_push($cTerms[$in->parent], $newdata);
-        //$cTerms[$in->parent][] = $newdata;
-        $cTerms[$in->parent]['childs'][] = $newdata;
-        //echo "has parent: " . $in->name . " " . $in->term_id . " has parent" . $in->parent . "<br>";
-      else:
-        $cTerms[$in->term_id]['name'] = $in->name;
-        $cTerms[$in->term_id]['parent'] = 0;
-      endif;
-    endforeach;
-    
-    usort($cTerms, function($a, $b) {
-      return $a['name'] <=> $b['name'];
-    });
-    
-    if($cTerms):
-      foreach($cTerms as $cterm):
-        if($cterm['parent']):
-          usort($cterm['childs'], function($a, $b) {
-            return $a['name'] <=> $b['name'];
-          });
-        endif;
-      endforeach;
-    endif;
-    
-  
-  endif;
-  echo "<select>";
-  foreach($cTerms as $key => $menu):
-    
-    if($menu['parent']):
-      echo '<optgroup label="'.$menu['name'].'">';      
-      if($menu['childs']):
-       foreach($menu['childs'] as $child):
-         echo '<option value="'.$child['child_id'].'">'. $child['name'] .'</option>';
-       endforeach;
-       echo '</optgroup>';  
-      endif;
-    else:
-      echo '<option value="'.$key.'">'. $menu['name'] .'</option>';
-    endif;
+// get all products and their TERMS
 
+$args = array(
+    'hide_empty' => false, 
+    'orderby' => 'name'
+);
+      
+// update post_type
+$posts_in_post_type = get_posts( array(
+    'fields' => 'ids',
+    'post_type' => 'custom_post_type',
+    'posts_per_page' => -1,
+    'suppress_filters' => 0
+) );
+
+// update custom_taxonomy 
+$ins = wp_get_object_terms( $posts_in_post_type, 'custom_taxonomy', array( 'ids' ) ); 
+
+// CREATE array to store the values
+$cTerms = array();
+
+  
+if($ins):
+
+  // First add all parents and also a bool to check if it is a parent or not
+  foreach($ins as $in):
+    if($in->parent):
+      $cTerms[$in->parent]['name'] = get_term( $in->parent )->name;
+      $cTerms[$in->parent]['parent'] = 1;
+    endif;
   endforeach;
-    echo "</select>";
+
+  // add values - check if the value has a parent or not
+  // if has parent add it to [childs]
+  foreach($ins as $in):
+    if($in->parent):
+      $newdata = array($in->term_id => $in->name, "name" => $in->name, "child_id" => $in->term_id);
+      $cTerms[$in->parent]['childs'][] = $newdata;
+    else:
+      $cTerms[$in->term_id]['name'] = $in->name;
+      $cTerms[$in->term_id]['parent'] = 0;
+    endif;
+  endforeach;
+
+  // SORT Main Array
+  usort($cTerms, function($a, $b) {
+    return $a['name'] <=> $b['name'];
+  });
+
+  // SORT Childs
+  if($cTerms):
+    foreach($cTerms as $cterm):
+      if($cterm['parent']):
+        usort($cterm['childs'], function($a, $b) {
+          return $a['name'] <=> $b['name'];
+        });
+      endif;
+    endforeach;
+  endif;
+
+
+endif;
+
+// possible output as select with parent as optgroup header
+echo "<select>";
+foreach($cTerms as $key => $menu):
+
+  if($menu['parent']):
+    echo '<optgroup label="'.$menu['name'].'">';      
+    if($menu['childs']):
+     foreach($menu['childs'] as $child):
+       echo '<option value="'.$child['child_id'].'">'. $child['name'] .'</option>';
+     endforeach;
+     echo '</optgroup>';  
+    endif;
+  else:
+    echo '<option value="'.$key.'">'. $menu['name'] .'</option>';
+  endif;
+
+endforeach;
+echo "</select>";
